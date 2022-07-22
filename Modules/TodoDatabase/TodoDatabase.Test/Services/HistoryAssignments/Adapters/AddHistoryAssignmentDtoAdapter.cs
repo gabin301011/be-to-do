30b47fda2bcf69d7b31c04f6f1b3396e9cc74a81
gabin301011 <gabin301011@gmail.com>
2022-07-22 22:27:21 +0200

    # Add tests

---
+++ Modules/TodoDatabase/TodoDatabase.Test/Services/HistoryAssignments/Adapters/AddHistoryAssignmentDtoAdapter.cs
@@ -0,0 +1,14 @@
+﻿using TodoDatabase.Services.HistoryAssignments.Dtos;
+
+namespace TodoDatabase.Test.Services.HistoryAssignments.Adapters {
+    internal class AddHistoryAssignmentDtoAdapter : IAddHistoryAssignmentDto {
+        private readonly string action;
+        private readonly int assignmentId;
+        public AddHistoryAssignmentDtoAdapter(string action, int assignmentId) {
+            this.action = action;
+            this.assignmentId = assignmentId;
+        }
+        public string Action => action;
+        public int AssignmentId => assignmentId;
+    }
+}
---
+++ Modules/TodoDatabase/TodoDatabase.Test/Services/HistoryAssignments/AddTest.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using TodoDatabase.Database;
+using TodoDatabase.Database.Models;
+using TodoDatabase.Repositories.HistoryAssignments;
+using TodoDatabase.Services.HistoryAssignments;
+using TodoDatabase.Test.Services.HistoryAssignments.Adapters;
+using Xunit;
+
+namespace TodoDatabase.Test.Services.HistoryAssignments {
+    public class AddTest {
+        private const int RETRN_COUNT_ONE = 1;
+        private const int RETRN_COUNT_TWO = 2;
+
+        public DbContextOptions<TodoDbContext> InitDb() {
+            return new DbContextOptionsBuilder<TodoDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+        }
+
+        [Fact]
+        public void When_DbIsEmpty_Should_AddElementAndReturnOneElement() {
+            //Init
+            TodoDbContext dbContext = new TodoDbContext(InitDb());
+            var hisotryAssignmentRepo = new HistoryAssignmentRepo(dbContext);
+            var historyAssignmentService = new HistoryAssignmentService(hisotryAssignmentRepo);
+            //Act
+            var addHistoryAssignmentDtoAdapter = new AddHistoryAssignmentDtoAdapter("TestAcrion", 200);
+            historyAssignmentService.Add(addHistoryAssignmentDtoAdapter);
+            var historyAssignmentsCount = dbContext.HistoryAssignments.Count();
+            //Assert
+            Assert.Equal(RETRN_COUNT_ONE, historyAssignmentsCount);
+        }
+
+        [Fact]
+        public void When_InDbExistOneElement_Should_AddElementAndReturnTwoElement()
+        {
+            //Init
+            TodoDbContext dbContext = new TodoDbContext(InitDb());
+            InitHistoryAssignmentOne(dbContext);
+            var hisotryAssignmentRepo = new HistoryAssignmentRepo(dbContext);
+            var historyAssignmentService = new HistoryAssignmentService(hisotryAssignmentRepo);
+            //Act
+            var addHistoryAssignmentDtoAdapter = new AddHistoryAssignmentDtoAdapter("TestAcrion", 200);
+            historyAssignmentService.Add(addHistoryAssignmentDtoAdapter);
+            var historyAssignmentsCount = dbContext.HistoryAssignments.Count();
+            //Assert
+            Assert.Equal(RETRN_COUNT_TWO, historyAssignmentsCount);
+        }
+
+        private void InitHistoryAssignmentOne(TodoDbContext dbContext) {
+            var oneHistoryAssignment = new HistoryAssignment {
+                Id = 1,
+                Action = "TestAction",
+                AssignmentId = 121,
+                Date = DateTime.Now
+            };
+            dbContext.HistoryAssignments.Add(oneHistoryAssignment);
+            dbContext.SaveChanges();
+        }
+    }
+}
